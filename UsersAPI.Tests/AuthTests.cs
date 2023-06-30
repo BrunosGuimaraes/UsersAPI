4c1da4ec7ea0cccfdec082379622fa596bff92ec
brunosguimaraes <brunos.guimaraes@outlook.com>
2023-06-30 18:59:11 -0300

    feat: Criação do DDD e Testes

---
+++ UsersAPI.Tests/AuthTests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+
+namespace UsersAPI.Tests
+{
+    public class AuthTests
+    {
+        [Fact(Skip = "Não implementado.")]
+        public void Auth_Post_Login_Returns_Ok()
+        {
+            //TODO
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public void Auth_Post_Login_Returns_Unathorized()
+        {
+            //TODO
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public void Auth_Post_Forgot_Password_Returns_Ok()
+        {
+            //TODO
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public void Auth_Post_Forgot_Password_Returns_BadRequest()
+        {
+            //TODO
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public void Auth_Post_Reset_Password_Returns_Ok()
+        {
+            //TODO
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public void Auth_Post_Reset_Password_Returns_BadRequest()
+        {
+            //TODO
+        }
+
+    }
+}
---
+++ UsersAPI.Tests/UsersTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UsersAPI.Tests
+{
+    public class UsersTests
+    {
+        [Fact(Skip ="Não implementado")]
+        public void Users_Post_Returns_Ok()
+        {
+
+        }
+
+        [Fact(Skip = "Não implementado")]
+        public void Users_Post_Returns_BadRequest()
+        {
+
+        }
+
+        [Fact(Skip = "Não implementado")]
+        public void Users_Put_Returns_Ok()
+        {
+
+        }
+
+        [Fact(Skip = "Não implementado")]
+        public void Users_Delete_Returns_Ok()
+        {
+
+        }
+
+        [Fact(Skip = "Não implementado")]
+        public void Users_Get_Returns_Ok()
+        {
+
+        }
+    }
+}
